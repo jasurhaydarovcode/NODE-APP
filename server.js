46c52482389b45042a07d3d5513f4c4990bf9648
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-25 00:33:26 +0500

    nodemon, uuid, dotenv packages added

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const http = require('http');
+const uuid = require('uuid');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
-    res.end('<h1>Response from server</h1>')
-    }else if(req.url === '/about'){
+    res.end(`<h1>Response from server</h1>`)
+    }
+    // About Page
+    else if(req.url === '/about'){
         res.end('<h1>About Page</h1>')
-    }else{
-        res.end('<h2>Page not found</h2>')
+    }
+    // Contact Page
+    else if(req.url === '/contact'){
+        res.end('<h1>Contact Page</h1>')
+    }
+    // 404 Page
+    else{
+        res.end('<body style="background: #000;"></body> <p style="color: #fff;">404 Page not found</p>')
     }
 });
+
+console.log(uuid.v4());
 
 const PORT = process.env.PORT || 2006;
 
