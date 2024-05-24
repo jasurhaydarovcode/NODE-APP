72495293a5f3488c985818a45206f6562999e0fa
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-25 01:54:07 +0500

    body parce

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
 const http = require('http');
-const uuid = require('uuid');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
-    res.end(`<h1>Response from server</h1>`)
-    }
-    // About Page
-    else if(req.url === '/about'){
-        res.end('<h1>About Page</h1>')
-    }
-    // Contact Page
-    else if(req.url === '/contact'){
-        res.end('<h1>Contact Page</h1>')
-    }
-    // 404 Page
-    else{
-        res.end('<body style="background: #000;"></body> <p style="color: #fff;">404 Page not found</p>')
-    }
-});
+    res.end(`
+        <html>
+            <head>
+                <title>NodeJs</title>
+            </head>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="message">
+                    <button type="submit">Send</button>
+                </form>
+            </body>
+        </html>
+    `)
+}
+    if(req.url === '/message' && req.method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
 
-console.log(uuid.v4());
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            fs.writeFileSync('message.txt', parsedBody.split('=')[1]);
+        })
+        res.end();
+    }
+    // // About Page
+    // else if(req.url === '/about'){
+    //     res.end('<h1>About Page</h1>')
+    // }
+    // // 404 Page
+    // else{
+    //     res.end('<body style="background: #000;"></body> <p style="color: #fff;">404 Page not found</p>')
+    // }
+});
 
 const PORT = process.env.PORT || 2006;
 
